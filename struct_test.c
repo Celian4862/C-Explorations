a34cabaed09e4ddcf1b65ff7af52b900c6402351
Celian4862 <galacticmermaid5times@protonmail.com>
2024-02-28 11:11:54 +0800

    Update struct_test.c

--- struct_test.c
+++ struct_test.c
@@ -6,13 +6,13 @@
 #define MAX_SIZE 100
 #endif
 
-void push(int*, int*);
-void pop(int*, int*);
-void print(int*, int*);
+void push(int*, int*, int*);
+void pop(int*, int*, int*);
+void print(int*, int*, int*);
 
 int main() {
-	int action, stack[MAX_SIZE], last_ind = 0;
-	void (*acts[3])(int*, int*) = {push, pop, print};
+	int action, *stack = (int*) malloc (4), size = 1, last_ind = 0;
+	void (*acts[3])(int*, int*, int*) = {push, pop, print};
 	do {
 		system("cls");
 		printf("Actions menu:\n1. Push\n2. Pop\n3. Show stack\n4. Exit\n\nInput action number: ");
@@ -21,33 +21,41 @@ int main() {
 			printf("Invalid choice.\n");
 			continue;
 		}
-		acts[action - 1](stack, &last_ind);
+		acts[action - 1](stack, &size, &last_ind);
 	} while (action != 4);
 	return 0;
 }
 
-void push(int stack[], int* last_ind) {
+void push(int *stack, int* size, int* last_ind) {
+	if (*size == MAX_SIZE) {
+		printf("Stack has reached maximum capacity.\n");
+		sleep(2,5);
+		return;
+	}
 	char char_input;
 	int input;
 	do {
 		printf("What would you like to push? Enter an integer here (and enter 'c' to cancel): ");
-		scanf("%c", &char_input);
-		if (isalpha(char_input)) {
-			printf("Invalid input. Enter an integer.\n");
-		} else if (char_input == 'c') {
+		scanf(" %c", &char_input);
+		if (char_input == 'c') {
 			printf("Push canceled.\n");
+			sleep(2,5);
 			return;
+		} else if (isalpha(char_input)) {
+			printf("Invalid input. Enter an integer.\n");
 		}
 	} while (isalpha(char_input));
 	input = atoi(&char_input);
 	stack[*last_ind] = input;
+	stack = (int*) realloc (stack, 4 * *size);
 	(*last_ind)++;
+	(*size)++;
 }
 
-void pop (int stack[], int* last_ind) {
-	return;
+void pop (int *stack, int* size, int* last_ind) {
+	
 }
 
-void print(int stack[], int* last_ind) {
+void print(int *stack, int* size, int* last_ind) {
 	return;
 }
