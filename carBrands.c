87d8e91f01ad45f6478ffe3cd2bc92c57afe3ad2
Celian4862 <galacticmermaid5times@protonmail.com>
2024-03-31 12:33:42 +0000

    Removed local-only compile files, created a test file

--- carBrands.c
+++ carBrands.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
-typedef struct Cars {
-    char plateNum[7];
-} Cars;
+// Define the struct for car details
+struct Car {
+    char plateNumber[20];
+    char model[50];
+    int modelYear;
+};
 
 int main() {
+    // Create an instance of the Car struct
+    struct Car myCar;
+
+    // Get user input for car details
+    printf("Enter car plate number: ");
+    scanf("%s", myCar.plateNumber);
+
+    printf("Enter car model: ");
+    scanf("%s", myCar.model);
+
+    printf("Enter car model year: ");
+    scanf("%d", &myCar.modelYear);
+
+    // Display the car details
+    printf("\nCar Details:\n");
+    printf("Plate Number: %s\n", myCar.plateNumber);
+    printf("Model: %s\n", myCar.model);
+    printf("Model Year: %d\n", myCar.modelYear);
+
     return 0;
 }
