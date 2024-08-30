34ab2194f2f08ebd48a2be8a1dbdb905f9564ad8
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-08-30 15:13:15 +0800

    Update student.h
    Added function definitions to student.h.

--- 2024.08.29 queue practice/student.h
+++ 2024.08.29 queue practice/student.h
@@ -19,4 +19,25 @@ Name createName(String, String);
 Student createStudent(int, Name, bool, String, int);
 void displayStudent(Student);
 
+Name createName(String fname, String lname) {
+    Name n;
+    strcpy(n.fname, fname);
+    strcpy(n.lname, lname);
+    return n;
+}
+
+Student createStudent(int studID, Name studName, bool sex, String program, int year) {
+    Student s;
+    s.studID = studID;
+    s.studName = studName;
+    s.sex = sex;
+    strcpy(s.program, program);
+    s.year = year;
+    return s;
+}
+
+void displayStudent(Student s) {
+    printf("%d | %s, %s | %c | %s | %d\n", s.studID, s.lname, s.fname, (s.sex) ? 'M' : 'F', s.program, s.year);
+}
+
 #endif
