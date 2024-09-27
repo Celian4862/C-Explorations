3b55693371ec4534ef67a37dbdd64d55edd6c0db
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-09-27 15:42:50 +0800

    Created function prototypes

--- HashADT/Closed hashing/Linear probing/hash.h
+++ HashADT/Closed hashing/Linear probing/hash.h
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 
-typedef enum flag {
+typedef enum {
     EMPTY,
     DELETED,
     OCCUPIED
@@ -14,26 +14,31 @@ typedef struct {
     char studName[30];
     char program[10];
     int year;
-    flag status;
 } Student;
 
 typedef struct {
-    Student *elem; // Dynamically allocate memory for rehashing
-    int count;
+    Student stud;
+    flag status;
+} Hash_idx;
+
+typedef struct {
+    Hash_idx *elem; // Dynamically allocate memory for rehashing
+    Student *stud_list; // Used when rehashing
+    int count, max;
 } HashTable;
 
 /*  Get the hash based on the summation of the first 3 letters of the name and the letters in the program.
     Assume that the name is always at least 3 letters long.
     The function must be able to return a value from 0 - 19. */
-int getHash(Student s);
+int getHash(const char*, const char*);
 
-void initDictionary(HashTable *ht);
+void initDictionary(HashTable*, int);
 // Use insertSorted if multiple data are already in the list.
-bool insert(HashTable *ht, Student s);
-bool deleteItem(HashTable *ht, Student s);
+bool insert(HashTable*, Student);
+bool deleteItem(HashTable*, Student);
 
-void visualiseTable(HashTable ht);
+void visualiseTable(HashTable);
 
-void freeHash(HashTable *ht);
+void freeHashTable(HashTable*);
 
 #endif
