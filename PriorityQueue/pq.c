5ede64f76701c6af23aef4687982bb61772b914b
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-10-19 00:31:58 +0800

    Updated pq.c

--- PriorityQueue/pq.c
+++ PriorityQueue/pq.c
@@ -3,23 +3,25 @@
 void (*display[])(Heap, int) = {breadth_first, preorder, inorder, postorder};
 
 bool initHeap(Heap *h) {
-    h->arr = (int*) malloc (sizeof(int) * MAX);
+    h->arr = (int*) malloc (sizeof(int) * (15));
     if (!h->arr) {
         printf("Memory allocation failed.\n");
         return false;
     }
-    h->max = MAX;
+    h->max = 15;
     h->lastIdx = -1;
+    h->depth_breadth = 8;
     return true;
 }
 
 bool resizeHeap(Heap *h) {
-    h->arr = (int*) realloc (h->arr, sizeof(int) * (h->max + MAX));
+    h->arr = (int*) realloc (h->arr, sizeof(int) * (h->max + h->depth_breadth * 2));
     if (!h->arr) {
         printf("Memory allocation failed.\n");
         return false;
     }
-    h->max = h->max + MAX;
+    h->depth_breadth *= 2;
+    h->max = h->max + h->depth_breadth;
     return true;
 }
 
@@ -42,15 +44,31 @@ bool rmHeap(Heap *h) {
     h->arr[0] = h->arr[h->lastIdx];
     h->arr[h->lastIdx--] = temp;
     // Sift down
-    int i = 0, left_child = i * 2 + 1, right_child = i * 2 + 2;
-    while (left_child < h->lastIdx && (h->arr[i] > h->arr[left_child] || h->arr[i] > h->arr[right_child])) {
-        if (h->arr[left_child] > h->arr[right_child]) {
-            h->arr[i] = temp;
-            h->arr[i] = h->arr[right_child];
-            h->arr[right_child] = temp;
-            i = right_child;
+    int i = 0, left_child = 1, right_child = 2;
+    temp = h->arr[0]; // Temp stores the data to sift down
+    while (left_child <= h->lastIdx) { // While left child exists
+        if (right_child <= h->lastIdx) { // If right child exists
+            if (temp > h->arr[left_child] || temp > h->arr[right_child]) { // If temp is greater than either of the children
+                if (h->arr[left_child] >= h->arr[right_child]) {
+                    h->arr[i] = h->arr[right_child];
+                    i = right_child;
+                } else if (h->arr[left_child] < h->arr[right_child]) {
+                    h->arr[i] = h->arr[left_child];
+                    i = left_child;
+                }
+                right_child = i * 2 + 2; // Right child changes to right child of the new node
+            } else {
+                break;
+            }
+        } else if (temp > h->arr[left_child]) {
+            h->arr[i] = h->arr[left_child];
+            i = left_child;
+        } else {
+            break;
         }
+        left_child = i * 2 + 1;
     }
+    h->arr[i] = temp;
     return false;
 }
 
