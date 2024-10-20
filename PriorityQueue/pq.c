6792fac56496a1d3ec9abd9da176e08a381576ab
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-10-20 22:36:39 +0800

    Updated PriorityQueue

--- PriorityQueue/pq.c
+++ PriorityQueue/pq.c
@@ -38,28 +38,42 @@ bool insertHeap(Heap *h, int data) {
 
 bool rmHeap(Heap *h) {
     if (h->lastIdx == -1) return false;
-
-    int last = h->arr[h->lastIdx],
-    i = 0,
-    left = i * 2 + 1,
-    smaller = (left + 1 <= h->lastIdx && h->arr[left + 1] < h->arr[left]) ? left + 1 : left;
-
+    int last = h->arr[h->lastIdx], i, smaller;
     h->arr[h->lastIdx--] = h->arr[0];
+    for (
+        i = 0,
+        smaller = (i * 2 + 2 <= h->lastIdx && h->arr[i * 2 + 2] < h->arr[i * 2 + 1]) ? i * 2 + 2 : i * 2 + 1;
 
-    while (left <= h->lastIdx && last > h->arr[smaller]) {
+        i * 2 + 1 <= h->lastIdx && last > h->arr[smaller];
+
         h->arr[i] = h->arr[smaller],
         i = smaller,
-        left = i * 2 + 1,
-        smaller = (left + 1 <= h->lastIdx && h->arr[left + 1] < h->arr[left]) ? left + 1 : left;
-    }
+        smaller = (i * 2 + 2 <= h->lastIdx && h->arr[i * 2 + 2] < h->arr[i * 2 + 1]) ? i * 2 + 2 : i * 2 + 1
+    ) {}
 
     h->arr[i] = last;
     return true;
 }
 
 bool heapify(Heap *h) {
-    
-    return false;
+    if (h->lastIdx == -1) return false;
+    for (int j = (h->lastIdx - 1) / 2; j >= 0; j--) {
+        int temp = h->arr[j],
+        smaller = (j * 2 + 2 <= h->lastIdx && h->arr[j * 2 + 1] < h->arr[j * 2 + 2]) ? j * 2 + 1 : j * 2 + 2;
+
+        int i;
+        for (
+            i = j;
+
+            i * 2 + 1 <= h->lastIdx && temp > h->arr[smaller];
+
+            h->arr[i] = h->arr[smaller],
+            i = smaller,
+            smaller = (i * 2 + 2 <= h->lastIdx && h->arr[i * 2 + 1] < h->arr[i * 2 + 2]) ? i * 2 + 1 : i * 2 + 2
+        ) {}
+        h->arr[i] = temp;
+    }
+    return true;
 }
 
 void breadth_first(Heap h, int idx) { // Must pass idx = 0 to display the entire tree
