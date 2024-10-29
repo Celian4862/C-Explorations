0f6a0a36017cd89e3f71ced0cc6d6ecd0d65c552
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-10-29 12:14:00 +0800

    Updated main.c

--- files/main.c
+++ files/main.c
@@ -2,11 +2,12 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
+    char s[] = "#include <stdio.h>\n\nint main(int argc, char **argv) {\n\tint x;\n\tprintf(\"Enter a number: \");\n\tscanf(\"\%d\", &x);\n\tprintf(\"The number is: \%d\\n\", x);\n\treturn 0;\n}";
     FILE *fp;
     fp = fopen("C:\\Users\\User\\Downloads\\C-Explorations\\files\\file_test.c", "w");
 
     if (fp) {
-        fwrite("#include <stdio.h>\n\nint main(int argc, char **argv) {\n\tprintf(\"Hello world!\\n\");\n\treturn 0;\n}", 1, 93, fp);
+        fwrite(s, 1, strlen(s), fp);
     }
 
     fclose(fp);
